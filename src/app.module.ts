2cde7f4cf26ac511236271c48e2f5ca796a01332
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2024-08-31 08:08:05 +0800

    add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeeModule } from './employee/employee.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EmployeeModule],
+  imports: [EmployeeModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
